7535e29bdb318e9ed97025f835c304d722e4de8a
“yanjiayingUk <1999jiulanyouji>
2020-03-04 12:03:56 +0800

    mocha-fe

---
+++ test/util.test.js
@@ -0,0 +1,7 @@
+var expect =chai.expect
+describe('矩形面积函数的测试',function(){
+    if('area(10,5)=50',function(){
+        var r=rectangle();
+        expect(r.area(10,5)).to.be.equal(50);
+    });
+});
